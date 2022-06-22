77a47669cf83696fd21523ab9e43ae2dd50f4caf
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-06-22 19:50:22 -0400

    Adicionado componente Header e add Theme em FormStep1

--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -1,20 +1,23 @@
 import { ReactNode } from 'react';
+import { Header } from '../Header';
 import * as C from './styles';
 
 type Props = {
   children: ReactNode;
 }
 
-export const Theme = ({Children} : Props) => {
+export const Theme = ({ children } : Props) => {
   return (
     <C.Container>
       <C.Area>
+        <Header />
+
         <C.Steps>
           <C.Sidebar>
             ...
           </C.Sidebar>
           <C.Page>
-            {Children}
+            {children}
           </C.Page>
         </C.Steps>
       </C.Area>
