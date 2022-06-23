b492c00f485fc5b08a509538b592302e68467dce
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-06-23 19:44:49 -0400

    Concluído página FormStep1

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,11 +1,28 @@
+import { useHistory } from 'react-router-dom';
 import * as C from './styles';
 import { Theme } from '../../components/Theme';
 
 export const FormStep1 = () => {
+
+  const history = useHistory();
+  const handleNextStep = () => {
+    history.push('/step2');
+  }
+
   return(
     <Theme>
       <C.Container>
-        Passo 1
+        <p>Passo 1/3</p>
+        <h1>Vamos começar com seu nome</h1>
+        <p>Preencha o campo abaixo com seu nome completo.</p>
+
+        <hr />
+
+        <label>
+          Nome Completo
+          <input type="text" autoFocus />
+        </label>
+        <button onClick={handleNextStep}>Próximo</button>
       </C.Container>
     </Theme>
   );
