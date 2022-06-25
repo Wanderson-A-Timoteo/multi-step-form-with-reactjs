d3ca8719b9a8a7a328c8f4e9346943890a2d2bf5
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-06-25 15:41:25 -0400

    Removido FormStep3 e substituido pelo FormStep1 pra reuso de código

--- src/pages/FormStep3/index.tsx
+++ src/pages/FormStep3/index.tsx
@@ -1,9 +1,56 @@
+import { useHistory } from 'react-router-dom';
 import * as C from './styles';
+import { useForm, FormActions } from '../../contexts/FormContext';
+import { Theme } from '../../components/Theme';
+import { ChangeEvent, useEffect } from 'react';
 
 export const FormStep3 = () => {
+
+  const history = useHistory();
+  const { state, dispatch } = useForm();
+
+  useEffect( () => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 1
+    });
+  }, []);
+
+  const handleNextStep = () => {
+    if (state.name !== '') {
+      history.push('/step2');
+    } else {
+      alert("Preencha todos os dados obrigatórios.")
+    }
+  }
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch( {
+      type: FormActions.setName,
+      payload: e.target.value
+    });
+  }
+
   return(
-    <C.Container>
-      Passo 3
-    </C.Container>
+    <Theme>
+      <C.Container>
+        <p>Passo 1/3</p>
+        <h1>Vamos começar com seu nome</h1>
+        <p>Preencha o campo abaixo com seu nome completo.</p>
+
+        <hr />
+
+        <label>
+          Nome Completo <span>*</span>
+          <input 
+            type="text" 
+            autoFocus 
+            value={state.name} 
+            onChange={handleNameChange}
+          />
+        </label>
+        <button onClick={handleNextStep}>Próximo</button>
+      </C.Container>
+    </Theme>
   );
 }
